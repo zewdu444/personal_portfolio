34c4fb99b6c50f5cc28e3c4e57f12b4b44e56b4b
zewdu erkyhun <zewdu444@gmail.com>
2022-12-19 17:45:08 +0300

    Fix eslinter error

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-const mobileoption = document.getElementById("mobileoption");
-const body = document.querySelector("body");
-window.addEventListener("resize", screenSize);
-mobileoption.style.display = "none";
-function openOption() {
-  mobileoption.style.display = "block";
-  body.style.overflow = "hidden";
-}
+const mobileoption = document.getElementById('mobileoption');
+const body = document.querySelector('body');
+window.addEventListener('resize', screenSize);
+const barClick = document.getElementById('barClick');
+mobileoption.style.display = 'none';
+
+// add event listener to click barclick
+barClick.addEventListener('click', () => {
+  mobileoption.style.display = 'block';
+  body.style.overflow = 'hidden';
+});
+
 // add event listener to mobile option
-mobileoption.addEventListener("click", function () {
-  mobileoption.style.display = "none";
-  body.style.overflow = "auto";
+mobileoption.addEventListener('click', () => {
+  mobileoption.style.display = 'none';
+  body.style.overflow = 'auto';
 });
 // check screen size and turned off mobile option in desktop
 function screenSize() {
   if (window.innerWidth >= 768) {
-    mobileoption.style.display = "none";
+    mobileoption.style.display = 'none';
   }
 }
+screenSize();
