df09e3267cff76bb821007c0fa2aa53346ee008c
zewdu erkyhun <zewdu444@gmail.com>
2022-12-19 17:02:20 +0300

    Add screen size listener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const mobileoption = document.getElementById("mobileoption");
 const body = document.querySelector("body");
-const content = document.querySelector(".content");
+window.addEventListener("resize", screenSize);
 mobileoption.style.display = "none";
 function openOption() {
   mobileoption.style.display = "block";
@@ -11,3 +11,9 @@ mobileoption.addEventListener("click", function () {
   mobileoption.style.display = "none";
   body.style.overflow = "auto";
 });
+// check screen size and turned off mobile option in desktop
+function screenSize() {
+  if (window.innerWidth >= 768) {
+    mobileoption.style.display = "none";
+  }
+}
