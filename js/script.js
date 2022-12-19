5a09dfc41b416bf20fbc9d13a1bb003a72a37b8b
zewdu erkyhun <zewdu444@gmail.com>
2022-12-19 15:40:17 +0300

    Modify desktop display

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mobileoption = document.getElementById("mobileoption");
 const body = document.querySelector("body");
 const content = document.querySelector(".content");
+mobileoption.style.display = "none";
 function openOption() {
   mobileoption.style.display = "block";
   body.style.overflow = "hidden";
