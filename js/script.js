5c7fad02894b99511bc1454d18a06add1ee62d6d
zewdu erkyhun <zewdu444@gmail.com>
2022-12-19 18:26:47 +0300

    Fix eslint configuration and js

--- js/script.js
+++ js/script.js
@@ -5,19 +5,18 @@ mobileoption.style.display = 'none';
 
 // add event listener to click barclick
 barClick.addEventListener('click', () => {
-    mobileoption.style.display = 'block';
-    body.style.overflow = 'hidden';
+  mobileoption.style.display = 'block';
+  body.style.overflow = 'hidden';
 });
 
 // add event listener to mobile option
 mobileoption.addEventListener('click', () => {
-    mobileoption.style.display = 'none';
-    body.style.overflow = 'auto';
+  mobileoption.style.display = 'none';
+  body.style.overflow = 'auto';
 });
 // check screen size and turned off mobile option in desktop
 window.addEventListener('resize', () => {
-    if (window.innerWidth >= 768) {
-        mobileoption.style.display = 'none';
-    }
-})
-
+  if (window.innerWidth >= 768) {
+    mobileoption.style.display = 'none';
+  }
+});
