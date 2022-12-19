b3c940a61954d2bd1d87354d3a11ada7c7fc4853
Usama Masood <113429825+UsamaMasood12@users.noreply.github.com>
2022-12-19 17:32:05 +0500

    Addition of function and event listener.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const mobileoption = document.getElementById("mobileoption");
 const body = document.querySelector("body");
 const content = document.querySelector(".content");
+function openOption() {
+  mobileoption.style.display = "block";
+  body.style.overflow = "hidden";
+}
+// add event listener to mobile option
+mobileoption.addEventListener("click", function () {
+  mobileoption.style.display = "none";
+  body.style.overflow = "auto";
+});
